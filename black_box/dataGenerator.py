08b7f2ac6f0bff0901e5e9500dc79f10bc29e5fb
Jayant Kalagnanam <jayant@Jayants-MacBook-Air.local>
2021-01-19 16:25:17 -0500

    better experiment

--- black_box/dataGenerator.py
+++ black_box/dataGenerator.py
@@ -12,11 +12,17 @@ def runLSMTree(is_compressed, query_path):
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
 
-    print(output, error)
     time = float(output.decode('utf-8').split('\n')[3].split(':')[1])
-    data_path = '../lsm_tree_compression/enc/'
-    data_size = sum(os.path.getsize(data_path + f) for f in os.listdir(data_path))
-    
+
+    # Get the sizes of the files and the compression ratio
+    if is_compressed != 0:
+        data_path = '../lsm_tree_compression/enc/'
+        data_size = get_size(data_path)
+    else:
+        data_path = '../lsm_tree_compression/data/'
+        data_size = get_size(data_path)
+
+    print(output, error, data_size)
     bash_command = "rm -rf data enc"
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     process.communicate()
@@ -29,7 +35,7 @@ def runLSMTree(is_compressed, query_path):
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     process.communicate()
 
-    return time
+    return time, data_size
 
 def createData(size, numberQueries, dataDistribution):
     os.chdir('../lsm_tree_compression')
@@ -40,10 +46,21 @@ def createData(size, numberQueries, dataDistribution):
     return None
 
 def compareCompressedToUncompressed(query_path):
-    compressed_time = runLSMTree(0, query_path)
-    uncompressed_time = runLSMTree(3, query_path)
+    compressed_time, compressed_size = runLSMTree(0, query_path)
+    uncompressed_time, uncompressed_size = runLSMTree(3, query_path)
 
-    return compressed_time, uncompressed_time
+    return compressed_time, uncompressed_time, compressed_size, uncompressed_size
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    
+    return total_size
 
 def createDataParameters(numEntries):
     #TODO: parametrize this and make it more random
@@ -62,12 +79,15 @@ def createDataParameters(numEntries):
     return size, numberQueries, dataDistribution
 
 def main():
-    numEntries = 50
+    numEntries = 1
     query_path = "queries.dsl"
     size, numberQueries, dataDistribution = createDataParameters(numEntries)
     #print(size, numberQueries, dataDistribution)
     compressed_times = []
     uncompressed_times = []
+    compressed_sizes = []
+    uncompressed_sizes = []
+    ratio = []
     y = []
     err_count = 0
 
@@ -75,9 +95,16 @@ def main():
         print("Entry: ", i, "Size: ", size[i], numberQueries[i], dataDistribution[i])
         createData(size[i], numberQueries[i], dataDistribution[i])
         try:
-            compressed_time, uncompressed_time = compareCompressedToUncompressed(query_path)
+            compressed_time, uncompressed_time, compressed_size, uncompressed_size = compareCompressedToUncompressed(query_path)
             compressed_times.append(compressed_time)
             uncompressed_times.append(uncompressed_time)
+            compressed_sizes.append(compressed_size)
+            uncompressed_sizes.append(uncompressed_size)
+
+            if uncompressed_size == compressed_size:
+                ratio.append(1.0)
+            else:
+                ratio.append(uncompressed_size / compressed_size)
             y.append(compressed_time > uncompressed_time)
         except IndexError:
             print("Some Error")
@@ -86,8 +113,10 @@ def main():
     print("Number Errors: ", err_count)
     now = datetime.now()
     file_name = now.strftime("%d%m%Y%H%M%S")
-    df = pandas.DataFrame(list(zip(size, numberQueries, dataDistribution, compressed_times, uncompressed_times, y)), 
-                            columns=['size', 'numberQueries', 'dataDistribution', 'compressed_time', 'uncompressed_time', 'y'])
+    df = pandas.DataFrame(list(zip(size, numberQueries, dataDistribution, compressed_times, 
+                                    uncompressed_times, compressed_sizes, uncompressed_sizes, ratio, y)), 
+                            columns=['size', 'numberQueries', 'dataDistribution', 
+                                        'compressed_time', 'uncompressed_time', 'compressed_size', 'uncompressed_size', 'ratio', 'y'])
     df.to_csv('../data/' + file_name + '.csv')
 
 if __name__ == '__main__':
